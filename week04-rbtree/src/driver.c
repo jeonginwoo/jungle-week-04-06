165e463710be3388479403230f7dcddc24286761
jeonginwoo <jiw413@naver.com>
2024-09-01 07:59:32 +0000

    rbtree insert 완성

--- week04-rbtree/src/driver.c
+++ week04-rbtree/src/driver.c
@@ -1,5 +1,44 @@
 #include "rbtree.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    rbtree *rb = new_rbtree();
+    rbtree_insert(rb, 45);
+    rbtree_insert(rb, 12);
+    rbtree_insert(rb, 89);
+    rbtree_insert(rb, 33);
+    rbtree_insert(rb, 76);
+    rbtree_insert(rb, 59);
+    rbtree_insert(rb, 8);
+    rbtree_insert(rb, 92);
+    rbtree_insert(rb, 23);
+    rbtree_insert(rb, 67);
+    rbtree_insert(rb, 18);
+    rbtree_insert(rb, 38);
+    rbtree_insert(rb, 54);
+    rbtree_insert(rb, 71);
+    rbtree_insert(rb, 29);
+    rbtree_insert(rb, 81);
+    rbtree_insert(rb, 5);
+    rbtree_insert(rb, 96);
+    rbtree_insert(rb, 50);
+    rbtree_insert(rb, 64);
 
+    rbtree_find(rb, 9);
+    rbtree_find(rb, 8);
+    rbtree_find(rb, 12);
+    rbtree_find(rb, 13);
+    rbtree_min(rb);
+    rbtree_max(rb);
+
+    key_t *arr = calloc(20, sizeof(key_t));
+    rbtree_to_array(rb, arr, (size_t)15);
+    for (int i=0; i<20; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    delete_rbtree(rb);
+    return 0;
 }
--- week04-rbtree/src/rbtree.c
+++ week04-rbtree/src/rbtree.c
@@ -3,9 +3,13 @@
 #include <stdio.h>
 
 
-void inorder(rbtree *t, node_t *node);
-void inorder_arr(node_t *nil, node_t *node, int* arr, int* idx);
-void postorder_free(rbtree *t, node_t *node);
+node_t *bst_insert(rbtree *t, const key_t key);     // 이진탐색트리 삽입
+void rbtree_check(rbtree *t, node_t *check);   // rbtree 규칙 확인
+void rotateRight(rbtree *t, node_t *z);     // 트리 오른쪽으로 회전
+void rotateLeft(rbtree *t, node_t *z);      // 트리 왼쪽으로 회전
+void inorder_print(rbtree *t, node_t *node);        // inorder 형태로 트리 출력
+void inorder_arr(node_t *nil, node_t *node, int* arr, int* idx);    // inorder 형태로 배열에 저장
+void postorder_free(rbtree *t, node_t *node);   // postorder 형태로 트리의 모든 노드 메모리 해제
 
 
 rbtree *new_rbtree(void)
@@ -14,6 +18,7 @@ rbtree *new_rbtree(void)
     // TODO: initialize struct if needed
     p->nil = (node_t *)calloc(1, sizeof(node_t));
     p->nil->color = RBTREE_BLACK;
+    p->root = p->nil;
     return p;
 }
 
@@ -27,38 +32,14 @@ void delete_rbtree(rbtree *t)
 node_t *rbtree_insert(rbtree *t, const key_t key)
 {
     // TODO: implement insert
-    node_t *insert_node = (node_t *)calloc(1, sizeof(node_t));
-    insert_node->key = key;
-    insert_node->color = RBTREE_RED;
-    insert_node->right = insert_node->left = t->nil;
-
-    if (t->root == NULL){
-        insert_node->color = RBTREE_BLACK;
-        t->root = insert_node;
-    } else {
-        node_t *now_node = t->root;
-        node_t *prev_node = NULL;
-        while (now_node != t->nil) {
-            prev_node = now_node;
-            if (key <= now_node->key){
-                now_node = now_node->left;
-            } else{
-                now_node = now_node->right;
-            }
-        }
-
-        if (key <= prev_node->key)
-            prev_node->left = insert_node;
-        else
-            prev_node->right = insert_node;
-        insert_node->parent = prev_node;
-    }
+    node_t *insert = bst_insert(t, key);
+    rbtree_check(t, insert);
 
     printf("insert %d\t: ", key);
-    inorder(t, t->root);
+    inorder_print(t, t->root);
     printf("\n");
 
-    return insert_node;
+    return insert;
 }
 
 node_t *rbtree_find(const rbtree *t, const key_t key)
@@ -74,9 +55,9 @@ node_t *rbtree_find(const rbtree *t, const key_t key)
     }
 
     if (find_node != t->nil) {
-        printf("find %d\t\t: find it!\n", key);
+        printf("find %d\t: find it!\n", key);
     } else {
-        printf("find %d\t\t: not exist\n", key);
+        printf("find %d\t: not exist\n", key);
     }
 
     return find_node;
@@ -136,12 +117,128 @@ int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n)
 
 //-----------------------------
 
-void inorder(rbtree *t, node_t *node) {
+node_t *bst_insert(rbtree *t, const key_t key) {
+    node_t *insert_node = (node_t *)calloc(1, sizeof(node_t));
+    insert_node->key = key;
+    insert_node->color = RBTREE_RED;
+    insert_node->right = insert_node->left = t->nil;
+
+    if (t->root == t->nil){
+        insert_node->color = RBTREE_BLACK;
+        t->root = insert_node;
+    } else {
+        node_t *now_node = t->root;
+        node_t *prev_node = t->nil;
+        while (now_node != t->nil) {
+            prev_node = now_node;
+            if (key <= now_node->key){
+                now_node = now_node->left;
+            } else{
+                now_node = now_node->right;
+            }
+        }
+
+        if (key <= prev_node->key)
+            prev_node->left = insert_node;
+        else
+            prev_node->right = insert_node;
+        insert_node->parent = prev_node;
+    }
+
+    return insert_node;
+}
+
+void rbtree_check(rbtree *t, node_t *check) {
+    if (check == t->root) {
+        check->color = RBTREE_BLACK;
+        return;
+    }
+    if (check->parent->color == RBTREE_BLACK){
+        return;
+    }
+    
+    node_t *parent = check->parent;
+    node_t *grand = parent->parent;
+    node_t *uncle = (grand->left == parent)? grand->right : grand->left;
+    
+    if (uncle->color == RBTREE_RED) {
+        parent->color = uncle->color = RBTREE_BLACK;
+        grand->color = RBTREE_RED;
+        rbtree_check(t, grand);
+    } else {
+        grand->color = RBTREE_RED;
+        if (grand->left == parent) {
+            if (parent->left == check) {
+                parent->color = RBTREE_BLACK;
+                rotateRight(t, grand);
+                rbtree_check(t, parent);
+            } else {
+                check->color = RBTREE_RED;
+                rotateLeft(t, parent);
+                rotateRight(t, grand);
+                rbtree_check(t, check);
+            }
+        } else {
+            if (parent->right == check) {
+                parent->color = RBTREE_BLACK;
+                rotateLeft(t, grand);
+                rbtree_check(t, parent);
+            } else {
+                check->color = RBTREE_RED;
+                rotateRight(t, parent);
+                rotateLeft(t, grand);
+                rbtree_check(t, check);
+            }
+        }
+    }
+}
+
+void rotateRight(rbtree *t, node_t *z) {
+    if (z == NULL) return;
+    node_t *x = z->left;
+    if (x == NULL) return;
+    node_t *b = x->right;
+    x->parent = z->parent;
+    if (z->parent != NULL) {
+        if (z->parent->left == z) {
+            z->parent->left = x;
+        } else {
+            z->parent->right = x;
+        }
+    }
+    x->right = z;
+    z->parent = x;
+    z->left = b;
+    if (b != NULL) b->parent = z;
+    if (z == t->root) t->root = x;
+}
+
+void rotateLeft(rbtree *t, node_t *z) {
+    if (z==NULL) return;
+    node_t *x = z->right;
+    if (x==NULL) return;
+    node_t *b = x->left;
+    x->parent = z->parent;
+    if (z->parent != NULL) {
+        if (z->parent->left == z) {
+            z->parent->left = x;
+        } else {
+            z->parent->right = x;
+        }
+    }
+    x->left = z;
+    z->parent = x;
+    z->right = b;
+    if (b != NULL) b->parent = z;
+    if (z == t->root) t->root = x;
+}
+
+void inorder_print(rbtree *t, node_t *node) {
     if (node == t->nil)
         return;
-    inorder(t, node->left);
+    inorder_print(t, node->left);
     printf("%d ", node->key);
-    inorder(t, node->right);
+    inorder_print(t, node->right);
 }
 
 void inorder_arr(node_t *nil, node_t *node, int* arr, int* idx) {
@@ -158,35 +255,4 @@ void postorder_free(rbtree *t, node_t *node) {
     postorder_free(t, node->left);
     postorder_free(t, node->right);
     free(node);
-}
-
-int main() {
-    rbtree *rb = new_rbtree();
-    rbtree_insert(rb, 10);
-    rbtree_insert(rb, 4);
-    rbtree_insert(rb, 26);
-    rbtree_insert(rb, 12);
-    rbtree_insert(rb, 6);
-    rbtree_insert(rb, 9);
-    rbtree_insert(rb, 5);
-    rbtree_insert(rb, 11);
-    rbtree_insert(rb, 3);
-    rbtree_insert(rb, 31);
-    rbtree_insert(rb, 17);
-    rbtree_find(rb, 9);
-    rbtree_find(rb, 8);
-    rbtree_find(rb, 12);
-    rbtree_find(rb, 13);
-    rbtree_min(rb);
-    rbtree_max(rb);
-
-    key_t *arr = calloc(20, sizeof(key_t));
-    rbtree_to_array(rb, arr, (size_t)15);
-    for (int i=0; i<20; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    delete_rbtree(rb);
-    return 0;
 }
